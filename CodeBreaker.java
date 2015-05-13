a91aa6b824502a7ca336fd95c60d7389f2299806
Samy <zarour.samy@gmail.com>
2015-05-13 18:28:18 -0400

    finished exercice 4 "Letter Change"

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -19,22 +19,31 @@ class CodeBreaker {
       char temp;
       int i;
       for(i= 0; i<sen.length(); i++){
-        System.out.println(i +" : " +sen);
         temp = sen.charAt(i);
         if(temp<65 || (temp>90 && temp<97) || temp>122) break;
       }
-      System.out.println("out");
       if(i==sen.length()) return sen;
-      System.out.println("before");
       String result = longestWord(sen.substring(i+1));
-      System.out.println("after");
       if(i>=result.length()) result = sen.substring(0,i);
       return result;
+    }
+    String letterChanges(String str) {
+      char[] arr = str.toCharArray();
+      for(int i=0; i<str.length(); i++){
+        if(arr[i]==90 || arr[i]==122) arr[i]=65;
+        else if((arr[i]>=65 && arr[i]<90) || (arr[i]>=97 && arr[i]<122)) arr[i]++;
+        if(arr[i]==101) arr[i]=69;
+        if(arr[i]==105) arr[i]=73;
+        if(arr[i]==111) arr[i]=79;
+        if(arr[i]==117) arr[i]=85;
+      }
+      str = str.copyValueOf(arr);
+      return str;
     }
   public static void main(String[] args){
     Scanner  s = new Scanner(System.in);
     CodeBreaker c = new CodeBreaker();
-    System.out.print(c.longestWord(s.nextLine()));
+    System.out.print(c.letterChanges(s.nextLine()));
   }
 
 }
