e5f79c5569f8b21fc432aa738a37ec000d27bb1b
Samy <zarour.samy@gmail.com>
2015-05-13 14:21:53 -0400

    finished exercice three "longest word function"

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -14,7 +14,27 @@ class CodeBreaker {
       if(num==0) return 1;
       return num*firstFactorial(num-1);
   }
+  String longestWord(String sen) {
+      if(sen.length()==0) return "";
+      char temp;
+      int i;
+      for(i= 0; i<sen.length(); i++){
+        System.out.println(i +" : " +sen);
+        temp = sen.charAt(i);
+        if(temp<65 || (temp>90 && temp<97) || temp>122) break;
+      }
+      System.out.println("out");
+      if(i==sen.length()) return sen;
+      System.out.println("before");
+      String result = longestWord(sen.substring(i+1));
+      System.out.println("after");
+      if(i>=result.length()) result = sen.substring(0,i);
+      return result;
+    }
   public static void main(String[] args){
+    Scanner  s = new Scanner(System.in);
+    CodeBreaker c = new CodeBreaker();
+    System.out.print(c.longestWord(s.nextLine()));
   }
 
 }
