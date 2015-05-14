ecd780c50f6f50db9fc05262fe8f13e2a7793dae
Samy <zarour.samy@gmail.com>
2015-05-14 14:06:17 -0400

    finished exercice 12 "vowel counter"

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -82,10 +82,17 @@ class CodeBreaker {
     }
     return result;
   }
+  int vowelCount(String str) {
+    int result=0;
+    for(int i=0; i<str.length();i++){
+      if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='o' || str.charAt(i)=='i' || str.charAt(i)=='y' || str.charAt(i)=='u' || str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='O' || str.charAt(i)=='I' || str.charAt(i)=='Y' || str.charAt(i)=='U') result++;
+    }
+    return result;
+  }
   public static void main(String[] args){
     Scanner  s = new Scanner(System.in);
     CodeBreaker c = new CodeBreaker();
-    System.out.print(c.abCheck(s.nextLine()));
+    System.out.print(c.vowelCount(s.nextLine()));
   }
 
 }
